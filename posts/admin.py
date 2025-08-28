33a1e92ba8cf2ed1dc7dadd3ec5a866e8a32cfcb
daniel4191 <daniell8282@naver.com>
2025-08-28 21:28:51 +0900

    admin페이지의 posts에서 추가된 이미지 썸네일 확인

--- posts/admin.py
+++ posts/admin.py
@@ -1,13 +1,30 @@
+# 이 임포트는 admin단에서 썸네일 보여주기 위해서 필요
+import admin_thumbnails
+
 from django.contrib import admin
+
 from .models import Post, PostImage, Comment
 
 # Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin_thumbnails.thumbnail('photo')
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+    
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         "id", 
         "content"
     ]
+    inlines = [
+        CommentInline,
+        PostImageInline
+        ]
     
 @admin.register(PostImage)
 class PostImageAdmin(admin.ModelAdmin):
@@ -24,3 +41,4 @@ class CommentAdmin(admin.ModelAdmin):
         "post",
         "content"
     ]
+
