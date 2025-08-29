4d2c37fe78763b5589e2a4d60b116a78c42207df
daniel4191 <daniell8282@naver.com>
2025-08-29 17:47:29 +0900

    작성자 일치여부에 따른 댓글 삭제기능

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import feeds
+from .views import feeds, comment_add, comment_delete, post_add
 
 urlpatterns = [
-    path("feeds/", feeds)
+    path("feeds/", feeds),
+    path("comment_add/", comment_add),
+    # views에서 comment = Comment.obejcts.get(id = comment_id)
+    # 이렇게 받아주기 때문에 comment_id를 사용하는 것이다.
+    path("comment_delete/<int:comment_id>/",comment_delete),
+    path("post_add/", post_add)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+
+from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
 def feeds(request):
@@ -13,6 +18,47 @@ def feeds(request):
     
     # 모든 글 목록을 템플릿으로 전달
     posts = Post.objects.all()
-    context = {"posts":posts}
+    comment_form = CommentForm()
+    context = {
+        "posts":posts,
+        "comment_form": comment_form
+        }
     return render(request, "posts/feeds.html", context)
 
+@require_POST
+@login_required(login_url="/users/login/")
+def comment_add(request):
+    #Commment 인스턴스 생성
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        # 메모리 상에 Comment 객체 생성
+        comment = form.save(commit = False)
+        
+        #Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+        # 사용유저 자동입력기능
+        comment.user = request.user
+
+        # DB에 Comment 객체 저장
+        comment.save()
+        
+        # 생성된 Comment 정보 확인
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+        
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+    
+@require_POST
+def comment_delete(request, comment_id):
+    if request.method == "POST":
+        comment = Comment.objects.get(id = comment_id)
+        if comment.user == request.user:
+            comment.delete()
+            return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+        else:
+            return HttpResponseForbidden("이 댓글을 삭제할 권한이 없습니다.")
+        
+def post_add(request):
+    return render(request, "posts/post_add.html")
+    
+    
